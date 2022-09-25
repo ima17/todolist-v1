ef3b4724d64d780bfc5b3b7745d332abcdc9b3c5
ima17 <ima.sudaraka@outlook.com>
2022-09-25 22:30:32 +0530

    Connect the Database

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!",
+});
+
+const item2 = new Item({
+  name: "Hit the + icon to add a new item",
+});
+
+const item3 = new Item({
+  name: "<- Hit this to delete an item",
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully saved default items to the database");
+  }
+});
 
 app.get("/", function (req, res) {
   const day = date.getDate();
